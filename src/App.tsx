6f4d3deb9e207e2c93f80763e27db6f7a532d584
jayptl11 <phamlong2k3@gmail.com>
2026-03-09 18:47:34 +0700

    fix white space

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Suspense } from 'react'
 import { RouterProvider } from 'react-router-dom'
 import Providers from './components/Providers'
 import GlobalNotifications from './components/GlobalNotifications'
@@ -7,7 +7,9 @@ import router from './router'
 function App() {
   return (
     <Providers>
-      <RouterProvider router={router} />
+      <Suspense fallback={<div className="flex h-screen w-screen items-center justify-center">Loading...</div>}>
+        <RouterProvider router={router} />
+      </Suspense>
       <GlobalNotifications />
     </Providers>
   )
